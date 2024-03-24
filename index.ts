b5f68a03f950c292a844e806fdf5188c5aaf9a50
NamPE286 <nambuihung654@gmail.com>
2024-03-25 01:13:42 +0700

    swagger

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import levelRoute from './routes/level.ts'
 import playerRoute from './routes/player.ts'
+import swaggerDocs from '@root/utils/swagger.ts'
 
 const app = express()
 
@@ -17,4 +18,5 @@ app.use('/player', playerRoute)
 
 app.listen(process.env.EXPRESS_PORT, () => {
     console.log(`Server started on port ${process.env.EXPRESS_PORT} (local url: http://localhost:${process.env.EXPRESS_PORT})`)
+    swaggerDocs(app, parseInt(process.env.EXPRESS_PORT!))
 })
---
+++ utils/swagger.ts
@@ -0,0 +1,45 @@
+import type { Express, Request, Response } from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "REST API Docs",
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["**/*.ts"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+function swaggerDocs(app: Express, port: number) {
+  // Swagger page
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+  // Docs in JSON format
+  app.get("/docs.json", (req: Request, res: Response) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+
+  console.log(`Docs available at http://localhost:${port}/docs`);
+}
+
+export default swaggerDocs;
