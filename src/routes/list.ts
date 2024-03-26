9763792dc34d72039a11e8357acda25b87862f73
NamPE286 <nambuihung654@gmail.com>
2024-03-26 10:26:02 +0700

    docs

--- src/routes/list.ts
+++ src/routes/list.ts
@@ -4,11 +4,97 @@ import { getDemonListLevels, getFeaturedListLevels } from '@lib/client'
 const router = express.Router()
 
 router.route('/DL')
+    /**
+     * @openapi
+     * "/list/DL":
+     *   get:
+     *     tags:
+     *       - List
+     *     summary: Get all records of the Demon List
+     *     parameters:
+     *       - name: start
+     *         in: query
+     *         description: Range start index
+     *         required: false
+     *         schema:
+     *           type: number
+     *           default: 0
+     *       - name: end
+     *         in: query
+     *         description: Range end index
+     *         required: false
+     *         schema:
+     *           type: number
+     *           default: 50
+     *       - name: sortBy
+     *         in: query
+     *         description: Property of level to sort by
+     *         required: false
+     *         schema:
+     *           type: string
+     *           default: dlTop
+     *       - name: ascending
+     *         in: query
+     *         description: Sort ascending
+     *         required: false
+     *         schema:
+     *           type: boolean
+     *           default: false
+     *     responses:
+     *       200:
+     *         description: Success
+     *         content:
+     *           application/json:
+     *             schema:
+     */
     .get(async (req, res) => {
         res.send(await getDemonListLevels(req.query))
     })
 
 router.route('/FL')
+    /**
+     * @openapi
+     * "/list/FL":
+     *   get:
+     *     tags:
+     *       - List
+     *     summary: Get all records of the Demon List
+     *     parameters:
+     *       - name: start
+     *         in: query
+     *         description: Range start index
+     *         required: false
+     *         schema:
+     *           type: number
+     *           default: 0
+     *       - name: end
+     *         in: query
+     *         description: Range end index
+     *         required: false
+     *         schema:
+     *           type: number
+     *           default: 50
+     *       - name: sortBy
+     *         in: query
+     *         description: Property of level to sort by
+     *         required: false
+     *         schema:
+     *           type: string
+     *           default: flTop
+     *       - name: ascending
+     *         in: query
+     *         description: Sort ascending
+     *         required: false
+     *         schema:
+     *           type: boolean
+     *           default: false
+     *     responses:
+     *       200:
+     *         description: Success
+     *         content:
+     *           application/json:
+     *             schema:
+     */
     .get(async (req, res) => {
         res.send(await getFeaturedListLevels(req.query))
     })
