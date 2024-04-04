d5274efcd2ddc4957531f2a16c77d60035bf5ce7
NamPE286 <nambuihung654@gmail.com>
2024-04-04 08:09:46 +0700

    docs

--- src/routes/leaderboard.ts
+++ src/routes/leaderboard.ts
@@ -4,6 +4,49 @@ import { getDemonListLeaderboard, getFeaturedListLeaderboard } from '@src/lib/cl
 const router = express.Router()
 
 router.route('/dl')
+    /**
+     * @openapi
+     * "/leaderboard/dl":
+     *   get:
+     *     tags:
+     *       - Leaderboard
+     *     summary: Get leaderboard of Demon List
+     *     parameters:
+     *       - name: start
+     *         in: query
+     *         description: Range start index
+     *         required: false
+     *         schema:
+     *           type: number
+     *           default: 0
+     *       - name: end
+     *         in: query
+     *         description: Range end index
+     *         required: false
+     *         schema:
+     *           type: number
+     *           default: 50
+     *       - name: sortBy
+     *         in: query
+     *         description: Property of level to sort by
+     *         required: false
+     *         schema:
+     *           type: string
+     *           default: overallRank
+     *       - name: ascending
+     *         in: query
+     *         description: Sort ascending
+     *         required: false
+     *         schema:
+     *           type: boolean
+     *           default: false
+     *     responses:
+     *       200:
+     *         description: Success
+     *         content:
+     *           application/json:
+     *             schema:
+     */
     .get(async (req, res) => {
         try {
             res.send(await getDemonListLeaderboard(req.query))
@@ -13,6 +56,49 @@ router.route('/dl')
     })
 
 router.route('/fl')
+    /**
+     * @openapi
+     * "/leaderboard/fl":
+     *   get:
+     *     tags:
+     *       - Leaderboard
+     *     summary: Get leaderboard of Featured List
+     *     parameters:
+     *       - name: start
+     *         in: query
+     *         description: Range start index
+     *         required: false
+     *         schema:
+     *           type: number
+     *           default: 0
+     *       - name: end
+     *         in: query
+     *         description: Range end index
+     *         required: false
+     *         schema:
+     *           type: number
+     *           default: 50
+     *       - name: sortBy
+     *         in: query
+     *         description: Property of level to sort by
+     *         required: false
+     *         schema:
+     *           type: string
+     *           default: flrank
+     *       - name: ascending
+     *         in: query
+     *         description: Sort ascending
+     *         required: false
+     *         schema:
+     *           type: boolean
+     *           default: false
+     *     responses:
+     *       200:
+     *         description: Success
+     *         content:
+     *           application/json:
+     *             schema:
+     */
     .get(async (req, res) => {
         try {
             res.send(await getFeaturedListLeaderboard(req.query))
