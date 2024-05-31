7888900ad4dcda3c1fa8f9c581af47d295779dc3
NamPE286 <nambuihung654@gmail.com>
2024-05-31 11:01:54 +0700

    update

--- src/lib/client/getRecords.ts
+++ src/lib/client/getRecords.ts
@@ -1,6 +1,6 @@
 import supabase from '@database/supabase'
 
-export async function getRecords({ start = 0, end = 0, isChecked = false } = {}) {
+export async function getRecords({ start = 0, end = 50, isChecked = false } = {}) {
     if (typeof isChecked == 'string') {
         isChecked = (isChecked == 'true')
     }
