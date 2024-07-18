0f73c4f8fc677e75d9571ffef091a565c625fc94
NamPE286 <nambuihung654@gmail.com>
2024-07-19 02:56:17 +0700

    update

--- src/lib/client/getPlayerRecords.ts
+++ src/lib/client/getPlayerRecords.ts
@@ -1,19 +1,24 @@
 import supabase from "@src/database/supabase"
 import Record from "@src/lib/classes/Record"
 
-export async function getPlayerRecords(uid: string, { start = 0, end = 50, isChecked = true } = {}) {
-    if (typeof isChecked == 'string') {
-        isChecked = (isChecked == 'true')
+export async function getPlayerRecords(uid: string, { start = 0, end = 50, sortBy = 'pt', ascending = 'false', isChecked = 'true' } = {}) {
+    let query = supabase
+        .from('records')
+        .select('*, levels!inner(*)')
+        .eq('userid', uid)
+        .eq('isChecked', isChecked == 'true')
+
+    if (sortBy == 'pt') {
+        query = query
+            .order('dlPt', { ascending: ascending == 'true' })
+            .order('flPt', { ascending: ascending == 'true' })
+    } else {
+        query = query.order(sortBy, { ascending: ascending == 'true' })
     }
 
-    const { data, error } = await supabase
-        .from('records')
-        .select('*, levels(*)')
-        .eq('userid', uid)
-        .order('dlPt', { ascending: false })
-        .order('flPt', { ascending: false })
-        .eq('isChecked', isChecked)
-        .range(start, end)
+    query = query.range(start, end)
+
+    const { data, error } = await query
 
     if (error) {
         throw error
