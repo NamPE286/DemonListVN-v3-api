6a6fc39fc22f8957c95c5ccf88e5b53b0ea33267
NamPE286 <nambuihung654@gmail.com>
2024-08-29 00:54:50 +0700

    update

--- src/lib/client/getPlayerSubmissions.ts
+++ src/lib/client/getPlayerSubmissions.ts
@@ -1,11 +1,12 @@
 import supabase from "@src/database/supabase"
 
-export async function getPlayerSubmissions(uid: string) {
+export async function getPlayerSubmissions(uid: string, { start = '0', end = '50' } = {}) {
     const { data, error } = await supabase
         .from('records')
         .select('*, levels(*)')
         .eq('userid', uid)
         .eq('isChecked', false)
+        .range(parseInt(start), parseInt(end))
 
     if (error) {
         throw error
