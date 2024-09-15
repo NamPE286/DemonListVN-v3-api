975f153e3e381df76abe88ddf2acefa4549a50c7
NamPE286 <nambuihung654@gmail.com>
2024-09-15 18:07:47 +0700

    update

--- src/lib/client/event.ts
+++ src/lib/client/event.ts
@@ -45,3 +45,54 @@ export async function getOngoingEvents() {
 
     return res?.concat(data!)
 }
+
+export async function getEventProof(eventID: number, uid: string) {
+    const { data, error } = await supabase
+        .from('eventProofs')
+        .select('*')
+        .match({ eventID: eventID, userid: uid })
+        .limit(1)
+        .single()
+
+    if (error) {
+        throw error;
+    }
+
+    return data
+}
+
+export async function getEventProofs(eventID: number, { start = 0, end = 50, accepted = 'true' } = {}) {
+    const { data, error } = await supabase
+        .from('eventProofs')
+        .select('*')
+        .eq('eventID', eventID)
+        .eq('accepted', accepted == 'true')
+        .order('created_at', { ascending: true })
+        .range(start, end)
+
+    if (error) {
+        throw error
+    }
+
+    return data
+}
+
+export async function upsertEventProof(data: any) {
+    const { error } = await supabase
+        .from('eventProofs')
+        .upsert(data)
+
+    if (error) {
+        throw error
+    }
+}
+
+export async function insertEventProof(data: any) {
+    const { error } = await supabase
+        .from('eventProofs')
+        .insert(data)
+
+    if (error) {
+        throw error
+    }
+}
--- src/routes/event.ts
+++ src/routes/event.ts
@@ -1,9 +1,32 @@
 import express from 'express'
-import { getEvent } from '@src/lib/client/event'
+import { getEvent, getEventProof, getEventProofs, insertEventProof, upsertEventProof } from '@src/lib/client/event'
+import userAuth from '@src/middleware/userAuth'
+import adminAuth from '@src/middleware/adminAuth'
 
 const router = express.Router()
 
 router.route('/:id')
+    /**
+     * @openapi
+     * "/events/{id}":
+     *   get:
+     *     tags:
+     *       - Event
+     *     summary: Get event by ID
+     *     parameters:
+     *       - name: id
+     *         in: path
+     *         description: The id of the event
+     *         required: true
+     *         schema:
+     *           type: number
+     *     responses:
+     *       200:
+     *         description: Success
+     *         content:
+     *           application/json:
+     *             schema:
+     */
     .get(async (req, res) => {
         const { id } = req.params
 
@@ -11,6 +34,46 @@ router.route('/:id')
             res.send(await getEvent(parseInt(id)))
         } catch {
             res.status(500).send()
+        }
+    })
+
+router.route('/:id/proofs')
+    .get(async (req, res) => {
+        const { id } = req.params
+
+        try {
+            res.send(await getEventProofs(parseInt(id), req.query))
+        } catch {
+            res.status(500).send();
+        }
+    })
+
+router.route('/:id/proof/:uid')
+    .get(async (req, res) => {
+        const { id, uid } = req.params
+
+        try {
+            res.send(await getEventProof(parseInt(id), uid))
+        } catch {
+            res.status(500).send();
+        }
+    })
+
+router.route('/proof')
+    .put(adminAuth, async (req, res) => {
+        try {
+            res.send(await upsertEventProof(req.body))
+        } catch {
+            res.status(500).send();
+        }
+    })
+    .post(userAuth, async (req, res) => {
+        req.body.userid = res.locals.user.data.uid
+        
+        try {
+            res.send(await insertEventProof(req.body))
+        } catch {
+            res.status(500).send();
         }
     })
 
