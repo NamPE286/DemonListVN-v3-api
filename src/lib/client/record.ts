2a85278a30b6f153aa6f7563e1a0dd7c1b20ae23
NamPE286 <nambuihung654@gmail.com>
2025-06-06 21:19:05 +0700

    remove order

--- src/lib/client/record.ts
+++ src/lib/client/record.ts
@@ -136,7 +136,6 @@ export async function retrieveRecord(user: Player) {
         .eq('needMod', false)
         .eq('isChecked', false)
         .eq('reviewer', user.uid!)
-        .order('queueNo', { ascending: true })
         .limit(1)
         .single()
 
