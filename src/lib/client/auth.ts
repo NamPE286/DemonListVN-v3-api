491c02e83719620b5271e49e5c5304748db6f586
NamPE286 <nambuihung654@gmail.com>
2025-06-08 18:18:51 +0700

    upd discord client

--- src/lib/client/auth.ts
+++
@@ -1,3 +0,0 @@
-export async function getDiscordAccessToken(code: string) {
-    // TODO
-}
--- src/lib/client/discord.ts
+++ src/lib/client/discord.ts
@@ -1,7 +1,47 @@
-export async function getUserByID(id: number) {
+export async function getUserIDFromAuthCode(code: string) {
     // TODO
 }
 
-export async function sendDirectMessage(userID: number, content: string) {
-    // TODO
+export async function getUserByID(id: number) {
+    const response = await fetch(`https://discord.com/api/v10/users/${id}`, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+            "Content-Type": "application/json"
+        }
+    });
+    const data = await response.json();
+
+    return data;
+}
+
+export async function createDirectMessageChannel(userID: number): Promise<number> {
+    const response = await fetch("https://discord.com/api/v10/users/@me/channels", {
+        method: "POST",
+        body: JSON.stringify({
+            "recipient_id": userID
+        }),
+        headers: {
+            "Authorization": `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+            "Content-Type": "application/json"
+        }
+    });
+    const data: any = await response.json();
+
+    return data.id;
+}
+
+export async function sendDirectMessage(channelID: number, content: string) {
+    const response = await fetch(`https://discord.com/api/v10/channels/${channelID}/messages`, {
+        method: "POST",
+        body: JSON.stringify({
+            "content": content
+        }),
+        headers: {
+            "Authorization": `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+            "Content-Type": "application/json"
+        }
+    });
+
+    await response.json();
 }
