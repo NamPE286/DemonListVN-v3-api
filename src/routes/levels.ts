0dae80e22a67e32f91e5644f7583b15b0c3c6b9f
NamPE286 <nambuihung654@gmail.com>
2025-06-13 07:54:37 +0700

    docs

--- src/routes/levels.ts
+++ src/routes/levels.ts
@@ -3,6 +3,23 @@ import supabase from '@src/database/supabase'
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * /levels/new:
+ *   get:
+ *     summary: Retrieve new levels with null ratings, flTop, and insaneTier.
+ *     tags:
+ *       - Levels
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved levels.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *       500:
+ *         description: Internal server error.
+ */
 router.route("/new")
     .get(async (req, res) => {
         const { data, error } = await supabase
