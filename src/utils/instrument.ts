e4e9c0785c749db26ef1941059e6a2ddf1a734c7
NamPE286 <nambuihung654@gmail.com>
2025-08-02 03:15:13 +0700

    upd

--- src/utils/instrument.ts
+++ src/utils/instrument.ts
@@ -1,6 +1,6 @@
 import * as Sentry from '@sentry/node'
 
 Sentry.init({
-  dsn: "https://a051a3204dbf41eb600f322b67dd4cdc@o4509770173054976.ingest.us.sentry.io/4509770259365888",
+  dsn: process.env.SENTRY_DSN,
   sendDefaultPii: true,
 });
