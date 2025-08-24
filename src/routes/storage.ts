8828ab3972e3e13212ead784dfeb3ff358e9b059
NamPE286 <nambuihung654@gmail.com>
2025-08-24 07:27:20 +0700

    auth

--- src/routes/storage.ts
+++ src/routes/storage.ts
@@ -3,13 +3,31 @@ import express from 'express'
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { s3 } from '@src/lib/classes/s3';
+import type Player from '@src/lib/classes/Player';
+
+function extractID(path: string) {
+    return path.split('/')[1].split('.')[0]
+}
+
+function validate(path: string, user: Player) {
+    if (path.startsWith('avatars') && user.uid == extractID(path)) {
+        return;
+    }
+
+    if (path.startsWith('clan-photos') && user.clan == parseInt(extractID(path))) {
+        return;
+    }
+
+    throw new Error('Forbidden')
+}
 
 const router = express.Router()
 
 router.route('/presign')
     .get(userAuth, async (req, res) => {
-        const { path } = req.query
-        let { bucket } = req.query
+        const { user } = res.locals
+        const { path } = req.query as { path: string }
+        let { bucket } = req.query as { bucket?: string }
 
         if (!bucket) {
             bucket = 'cdn'
@@ -24,17 +42,22 @@ router.route('/presign')
         }
 
         const command = new PutObjectCommand({
-            Bucket: String(bucket),
-            Key: String(path)
+            Bucket: bucket,
+            Key: path
         })
 
-        // TODO: Auth for different path
+        try {
+            validate(path, user)
+        } catch {
+            res.status(403).send()
+            return
+        }
 
         try {
-            res.send(await getSignedUrl(s3, command, { expiresIn: 120 }))
+            res.send(await getSignedUrl(s3, command, { expiresIn: 120 }));
         } catch (err) {
-            console.error(err)
-            res.status(500).send()
+            console.error(err);
+            res.status(500).send();
         }
     })
 
