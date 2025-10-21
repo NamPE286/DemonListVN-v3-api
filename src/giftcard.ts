78a894ddfc8dbcea015ce50f9e5e02973fc666d1
NamPE286 <nambuihung654@gmail.com>
2025-10-21 22:57:22 +0700

    TODO: giftcard

---
+++ src/giftcard.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.route('/:code')
+    .get(async (res, req) => {
+        // TODO
+    })
+
+router.route('/:code')
+    .patch(async (res, req) => {
+        // TODO
+    })
+
+export default router
