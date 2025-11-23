051efcbf520e162ff9108a6b7bb7a6059c6ce95c
NamPE286 <nambuihung654@gmail.com>
2025-11-24 03:54:22 +0700

    cloudflare

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import swaggerDocs from '@src/utils/swagger.ts'
 import { version } from '../package.json'
 import cron from 'node-cron'
 import supabase from '@src/database/supabase'
+import { httpServerHandler } from "cloudflare:node";
 
 import levelRoute from './routes/level'
 import listRoute from './routes/list'
@@ -111,3 +112,5 @@ app.listen(port, () => {
     console.log('Timezone: ' + Intl.DateTimeFormat().resolvedOptions().timeZone)
     swaggerDocs(app, port)
 })
+
+export default httpServerHandler({ port: port });
