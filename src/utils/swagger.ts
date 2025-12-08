8e395c2a8a84c367da45b7cf44825d89aaf39e1e
NamPE286 <nambuihung654@gmail.com>
2025-12-09 04:06:40 +0700

    update

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -3,7 +3,6 @@ import { swaggerHtml } from "../../static/swagger-html";
 
 async function swaggerDocs(app: Express, port: number) {
   try {
-    // Swagger page - serve pre-generated HTML
     app.get("/docs", (req: Request, res: Response) => {
       res.setHeader("Content-Type", "text/html");
       res.send(swaggerHtml);
@@ -11,7 +10,6 @@ async function swaggerDocs(app: Express, port: number) {
 
     console.log(`Docs available at /docs`);
   } catch (error) {
-    // Swagger UI not available
     console.log('Swagger UI not available in this environment');
     console.error(error);
   }
